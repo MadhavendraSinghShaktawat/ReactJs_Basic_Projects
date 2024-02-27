db9b7d7948e66190a34f2fe7d97183be5cf06b0f
MadhavendraSinghShaktawat <madhavendrasinghshaktawat@gmail.com>
2024-02-27 21:16:07 +0530

    adding DigitalClock-app

---
+++ StopWatch-app/src/StopWatch.jsx
@@ -0,0 +1,44 @@
+import React, {useState, useEffect} from "react";
+
+function StopWatch(){
+
+    const [time, setTime] = useState(new Date());
+
+    useEffect(() => {
+
+        const intervalId = setInterval(() => {
+            setTime(new Date());
+        }, 1000)
+
+        return() => {
+            clearInterval(intervalId);
+        }
+
+    }, []);
+
+    function formatTime(){
+        let hours = time.getHours();
+        const minutes = time.getMinutes();
+        const seconds = time.getSeconds();
+        const meridian = hours >= 12 ? "PM" : "AM";
+
+        hours = hours % 12 || 12;
+
+        return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)} ${padZero(meridian)}`
+    }
+
+    function padZero(n){
+            return(n < 10 ? "0" : "") + n;
+    }
+
+
+    return(
+        <div className="clock-container">
+            <div className="clock">
+                <span>{formatTime()}</span>
+            </div>
+        </div>
+    );
+}
+
+export default StopWatch
